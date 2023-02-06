11886f29375f983cca83a96c4c39679a432fa85f
yuzhiwei <Sc123456!>
2023-02-06 15:32:33 +0800

    feature(Helloword):

---
+++ exercise/src/coding/HelloWord.java
@@ -0,0 +1,12 @@
+package coding;
+
+/**
+ * @author: yuzhiwei
+ * @description:
+ * @create: 2023/2/6 15:30
+ **/
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
