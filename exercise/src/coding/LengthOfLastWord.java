0813a8d0357aff25fdac8d14e89f685626a81ff2
yuzhiwei <Sc123456!>
2023-02-08 10:48:53 +0800

    feature(最后一个单词的长度):

---
+++ exercise/src/coding/LengthOfLastWord.java
@@ -0,0 +1,23 @@
+package coding;
+
+/**
+ * @author: yuzhiwei
+ * @description:
+ * @create: 2023/2/8 10:43
+ **/
+public class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        int end = s.length() - 1;
+
+        while (end >= 0 && s.charAt(end) == ' '){
+            end--;
+        }
+
+        int start = end;
+        while (start >= 0 && s.charAt(start) != ' '){
+            start--;
+        }
+
+        return end - start;
+    }
+}
