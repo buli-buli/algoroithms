1dd4c3ded73a3efefb17ebf889da7c150358a2ed
yuzhiwei <Sc123456!>
2023-07-04 09:51:09 +0800

    feature(工具类):初始化TreeNode对象的工具类

---
+++ exercise/src/coding/tree/easy/BinaryTreeConstruction.java
@@ -0,0 +1,29 @@
+package coding.tree.easy;
+
+public class BinaryTreeConstruction {
+    public static TreeNode constructBinaryTree(Integer[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+
+        return constructTreeHelper(nums, 0);
+    }
+
+    private static TreeNode constructTreeHelper(Integer[] nums, int index) {
+        if (index >= nums.length || nums[index] == null) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(nums[index]);
+        node.left = constructTreeHelper(nums, 2 * index + 1);
+        node.right = constructTreeHelper(nums, 2 * index + 2);
+
+        return node;
+    }
+
+    public static void main(String[] args) {
+        Integer[] nums = {1, 2, 2, 3, 3, null, null, 4, 4};
+        TreeNode root = constructBinaryTree(nums);
+        // 可以对构造出来的二叉树进行其他操作或遍历验证
+    }
+}
