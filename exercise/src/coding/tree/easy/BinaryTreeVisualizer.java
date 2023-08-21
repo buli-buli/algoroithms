bcfaaf674d5e55d039154bb19d8ad67ce98fe217
yuzhiwei <Sc123456!>
2023-08-21 14:15:05 +0800

    feature(工具类):二叉树可视化

---
+++ exercise/src/coding/tree/easy/BinaryTreeVisualizer.java
@@ -0,0 +1,106 @@
+package coding.tree.easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinaryTreeVisualizer {
+    public static void visualize(TreeNode root) {
+        int maxLevel = maxLevel(root);
+
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static void printNodeInternal(List<TreeNode> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || isAllElementsNull(nodes)) {
+            return;
+        }
+
+        int floor = maxLevel - level;
+        int edgeLines = (int) Math.pow(2, Math.max(floor - 1, 0));
+        int firstSpaces = (int) Math.pow(2, floor) - 1;
+        int betweenSpaces = (int) Math.pow(2, floor + 1) - 1;
+
+        printSpaces(firstSpaces);
+
+        List<TreeNode> newNodes = new ArrayList<>();
+        for (TreeNode node : nodes) {
+            if (node != null) {
+                System.out.print(node.val);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            printSpaces(betweenSpaces);
+        }
+        System.out.println();
+
+        for (int i = 1; i <= edgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                printSpaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    printSpaces(edgeLines + edgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).left != null) {
+                    System.out.print("/");
+                } else {
+                    printSpaces(1);
+                }
+
+                printSpaces(i + i - 1);
+
+                if (nodes.get(j).right != null) {
+                    System.out.print("\\");
+                } else {
+                    printSpaces(1);
+                }
+
+                printSpaces(edgeLines + edgeLines - i);
+            }
+            System.out.println();
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printSpaces(int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    private static int maxLevel(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+    }
+
+    private static boolean isAllElementsNull(List<TreeNode> list) {
+        for (TreeNode treeNode : list) {
+            if (treeNode != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        TreeNode root = new TreeNode(1,
+                new TreeNode(2,
+                        new TreeNode(4),
+                        new TreeNode(5)),
+                new TreeNode(3,
+                        new TreeNode(6),
+                        new TreeNode(7)));
+
+        visualize(root);
+    }
+}
