e954f55abed44ebecaad3cd17c639de759824a33
余志尉 <123@qq.com>
2026-02-04 16:36:57 +0800

    代码提交

---
+++ exercise/src/coding/dynamic/medium/MinPathSum.java
@@ -0,0 +1,36 @@
+package coding.dynamic.medium;
+
+import coding.MyUtil;
+
+/**
+ * @author yzw
+ * @Description TODO
+ * @createTime 2026年02月04日
+ */
+public class MinPathSum {
+    public static void main(String[] args) {
+        System.out.println(minPathSum(new int[][]{{1,3,1},{1,5,1},{4,2,1}}));
+    }
+    static int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+
+        dp[0][0] = grid[0][0];
+
+        for (int i = 1; i < m; i++){
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++){
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
